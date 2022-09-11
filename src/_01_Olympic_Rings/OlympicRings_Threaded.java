3e0d6f82b1d29e6757234f43afc5e1bf368b74d3
Nazarshia2889 <arshia.nayebnazar@yahoo.com>
2022-09-11 11:25:40 -0700

    threads

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,58 @@
 package _01_Olympic_Rings;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class OlympicRings_Threaded {
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
-
+	public static void main(String[] args) {
+		Robot one = new Robot(10, 400);
+		Robot two = new Robot(300, 400);
+		Robot three = new Robot(590, 400);
+		Robot four = new Robot(880, 400);
+		
+		one.setSpeed(10);
+		two.setSpeed(10);
+		three.setSpeed(10);
+		four.setSpeed(10);
+		
+		Thread r1 = new Thread(() -> { 
+			int angle = 0;
+			one.penDown();
+			for(int i = 0; i < 360; i++) {
+				one.move(4);
+				one.setAngle(angle + i);
+			}
+		});
+		Thread r2 = new Thread(() -> { 
+			int angle = 0;
+			two.penDown();
+			for(int i = 0; i < 360; i++) {
+				two.move(4);
+				two.setAngle(angle + i);
+			}
+		});
+		Thread r3 = new Thread(() -> { 
+			int angle = 0;
+			three.penDown();
+			for(int i = 0; i < 360; i++) {
+				three.move(4);
+				three.setAngle(angle + i);
+			}
+		});
+		Thread r4 = new Thread(() -> { 
+			int angle = 0;
+			four.penDown();
+			for(int i = 0; i < 360; i++) {
+				four.move(4);
+				four.setAngle(angle + i);
+			}
+		});
+		
+		r1.start();
+		r2.start();
+		r3.start();
+		r4.start();
+	}
+	
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,55 @@
 package _02_Advanced_Robot_Race;
 
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class AdvancedRobotRace {
 	// Re-do the robot race recipe from level 3 module 0. 
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		Robot[] robots = new Robot[5];
+		Thread[] threads = new Thread[5];
+		int x = 100;
+		int y = 500;
+		Random rand = new Random();
+		boolean race = true;
+		
+		for(int i = 0;i<robots.length;i++) {
+			robots[i] = new Robot();
+			robots[i].setX(x);
+			robots[i].setY(y);
+			x += 150;
+			int a = i;
+			
+			threads[i] = new Thread(() -> {
+				while(robots[a].getY() > 50) {
+					int r = rand.nextInt(100);
+					robots[a].setSpeed(2);
+					robots[a].move(r);
+				}
+				System.exit(0);
+			});
+			
+		}
+		
+		for(int i = 0; i < threads.length; i++) {
+			threads[i].start();
+		}
+		
+		
+		
+//		while(race == true) {	
+//			for(int i = 0;i<robots.length;i++) {
+//				int r = rand.nextInt(50);
+//				robots[i].setSpeed(5);
+//				robots[i].move(r);	
+//				if(robots[i].getY()<=50) {
+//					race = false;
+//					robots[i].setSpeed(10);
+//					robots[i].turn(360);
+//				}
+//			}
+//		}
+	}
 }
